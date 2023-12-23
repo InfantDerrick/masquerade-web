2fc4000ba72bff7d077e36a002e8392d82edf602
Angeline Milton <angelinemilton00@gmail.com>
2023-12-23 17:42:44 -0600

    Added all img resources

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
     <main className={styles.main}>
       <h1 className={styles.title}>Coming Soon</h1>
       <p className={styles.description}>
-        We're working hard to bring you Masquerade. Stay tuned!
+        We&apos;re working hard to bring you Masquerade. Stay tuned!
       </p>
     </main>
   );
